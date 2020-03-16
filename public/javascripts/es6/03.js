318ca908be69352f4aa2f1eb41084d34ad8b1fed
Kelvin Chen <bdfd2005@gmail.com>
2020-03-16 18:49:23 -0400

    1

--- public/javascripts/es6/03.js
+++ public/javascripts/es6/03.js
@@ -100,5 +100,23 @@ function developSoftware(programmers) {
 developSoftware(programmers);
 
 class Animal {
-  constructor(name) {}
+  constructor(name) {
+    this.name = name;
+  }
+  makeSound() {
+    console.log('Generic Animal Sound.');
+  }
 }
+class Dog extends Animal {
+  constructor(name) {
+    super(name);
+  }
+  makeSound() {
+    super.makeSound();
+    console.log('Woof!Woof!');
+  }
+}
+const a1 = new Animal('Dom');
+a1.makeSound();
+const a2 = new Dog('Jeff');
+a2.makeSound();
